195a424babe4ad545090b4e58b1de63decccbf07
Blaarkies <pierre.roux6@gmail.com>
2026-05-24 17:15:43 +0200

    add regex sandbox

---
+++ src/app/page/talk/regex/sandbox-regex/sandbox-regex.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sandbox-regex',
+  imports: [],
+  templateUrl: './sandbox-regex.html',
+  styleUrl: './sandbox-regex.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class SandboxRegex {
+
+}
